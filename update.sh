73f885fda88e349ab5788049dbbf9880bce8a524
Lemonn <lemonn@protonmail.com>
2019-01-02 18:07:02 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Skript zum Updaten der Labboraufgabenkonfiguration
+# Funktioniert nur mir Internet innerhalb der VM!
+# Notfalls die Dateien von Hand auf dem Host herunterladen und in die VM kopieren.
+
+# Neue Konfuguration laden
+wget -P ~/update --no-check-certificate --content-disposition https://github.com/Lemonn/Knetze_Labor_Aufagabe4/blob/master/Aufgabe4-1.py
+wget -P ~/update --no-check-certificate --content-disposition https://github.com/Lemonn/Knetze_Labor_Aufagabe4/blob/master/Aufgabe4-2.py
+wget -P ~/update --no-check-certificate --content-disposition https://github.com/Lemonn/Knetze_Labor_Aufagabe4/blob/master/Aufgabe4-3.py
+
+# Alte Konfiguration Löschen
+cd ~/mininet/custom/
+rm Aufgabe4-1.py Aufgabe4-2.py Aufgabe4-3.py
+mv ~/update/Aufgabe4-1.py ~/mininet/custom/
+mv ~/update/Aufgabe4-2.py ~/mininet/custom/
+mv ~/update/Aufgabe4-3.py ~/mininet/custom/
 
 
-wget -q --spider http://google.com
-
-if [ $? -eq 0 ]; then
-    echo "Beginne Update"
-    wget -P ~/update --no-check-certificate --content-disposition https://github.com/Lemonn/Knetze_Labor_Aufagabe4/blob/master/Aufgabe4-1.py
-    if [ $? -eq 0 ]; then
-    
-    else
-        echo "Fehler ggf. keine Schreibrechte, die Mininet Konfigurationen können manuell von https://github.com/Lemonn/Knetze_Labor_Aufagabe4 Heruntergeldaen werden!"
-        exit
-else
-    echo "Keine Internetverbindung, die Mininet Konfigurationen können manuell von https://github.com/Lemonn/Knetze_Labor_Aufagabe4 Heruntergeldaen werden!"
-fi
-
-
-
+# Befehl zum Updaten
 # wget -P ~/update --no-check-certificate --content-disposition https://github.com/Lemonn/Knetze_Labor_Aufagabe4/blob/master/update.sh && chmod +x ~/update/update.sh && ~/update/update.sh
