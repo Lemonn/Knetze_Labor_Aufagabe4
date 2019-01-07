32e91d92cedb7ec690b89762ae075336d3db0bfb
Lemonn <lemonn@protonmail.com>
2019-01-07 01:44:37 +0100

    Update Aufgabe4-2.py

--- Aufgabe4-2.py
+++ Aufgabe4-2.py
@@ -26,7 +26,6 @@ class NetTopo( Topo ):
 	self.addLink(sv1, s2, bw=10)
 	self.addLink(sv1, s2, bw=10)
 	
-	# Netzwerkverbindngen ohne Limitierung, für die SSH verbidungen zu den Hosts
 	self.addLink(s3, c1)
 	self.addLink(s3, c2)
 	self.addLink(s3, sv1)
